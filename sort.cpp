9dea460818f3b7282de2f233cd48ffb2c817175c
Unknown <mjhaveri@bu.edu>
2018-02-06 14:06:59 -0500

    finished merge sort

--- sort.cpp
+++ sort.cpp
@@ -19,6 +19,7 @@ Sort<T>::~Sort()
     (*arr).clear();
     delete (this->arr);
 }
+
 template <class T>
 std::ostream &operator<<(std::ostream &os, const Sort<T> &sort)
 {
@@ -30,20 +31,71 @@ std::ostream &operator<<(std::ostream &os, const Sort<T> &sort)
 }
 
 template <class T>
+std::vector<T> Sort<T>::returnSortedVector()
+{
+    return (*(this->arr));
+}
+
+template <class T>
 MergeSort<T>::MergeSort() : Sort<T>()
 {
 }
 
 template <class T>
-MergeSort<T>::MergeSort(const std::vector<T> arr) : Sort<T>(arr) {}
+MergeSort<T>::MergeSort(const std::vector<T> arr) : Sort<T>(arr)
+{
+    int n = (*(this->arr)).size();
+    this->Mergesort(this->arr, 0, n - 1);
+}
+
+template <class T>
+MergeSort<T>::~MergeSort() {}
+
+template <class T>
+void MergeSort<T>::Mergesort(std::vector<T> *arr, int p, int r)
+{
+    if (p < r)
+    {
+        int q = (p + r) / 2;
+        Mergesort(arr, p, q);
+        Mergesort(arr, q + 1, r);
+        Merge(arr, p, q, r);
+    }
+}
+template <class T>
+void MergeSort<T>::Merge(std::vector<T> *arr, int p, int q, int r)
+{
+    int i = p, k = p;
+    int j = q + 1;
+    std::vector<T> tmp;
+    while ((i <= p) && (j <= r))
+    {
+        if ((*arr)[i] <= (*arr)[j])
+        {
+            tmp.push_back((*arr)[i++]);
+        }
+        else
+        {
+            tmp.push_back((*arr)[j++]);
+        }
+    }
+    while (i <= q)
+        tmp.push_back((*arr)[i++]);
+    while (j <= i)
+        tmp.push_back((*arr)[j++]);
+    for (i = p; i <= r; i++)
+    {
+        (*arr)[i] = tmp[i - p];
+    }
+}
 
 int main()
 {
     std::vector<int> test;
-    test.push_back(0);
-    test.push_back(1);
     test.push_back(2);
-    Sort<int> d(test);
+    test.push_back(1);
+    test.push_back(0);
+    MergeSort<int> d(test);
     std::cout << d;
     return 0;
 }
--- sort.hpp
+++ sort.hpp
@@ -9,11 +9,12 @@ public:
   Sort(const std::vector<T> arr);
   Sort();
   virtual ~Sort();
+  std::vector<T> returnSortedVector();
 
   template <typename U>
   friend std::ostream &operator<<(std::ostream &os, const Sort<U> &sort);
 
-private:
+protected:
   std::vector<T> *arr;
 };
 
@@ -23,5 +24,10 @@ class MergeSort : public Sort<T>
 public:
   MergeSort(const std::vector<T> arr);
   MergeSort();
+  ~MergeSort();
+
+private:
+  void Mergesort(std::vector<T> *arr, int p, int r);
+  void Merge(std::vector<T> *arr, int p, int q, int r);
 };
 #endif
