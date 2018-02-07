2e9c86e6aff32c468af1cf3f1f967a70ca0987cf
Unknown <mjhaveri@bu.edu>
2018-02-07 11:49:10 -0500

    finished problem 3

--- sortingProblem.cpp
+++ sortingProblem.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <vector>
 #include "sort.hpp"
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
 
 int countUniqueElements(std::vector<int> vec)
 {
-    int uElements = 0;
+    int uElements = 1;
     int elem = vec[0];
     for (auto i : vec)
     {
@@ -17,7 +19,60 @@ int countUniqueElements(std::vector<int> vec)
     return uElements;
 }
 
-void problem1()
+int findWinner(std::vector<int> vec)
+{
+    int maxVoter = 0;
+    int maxCount = 1;
+    int uId = vec[0];
+    int count = 1;
+    for (auto i : vec)
+    {
+        if (uId != i)
+        {
+            count = 1;
+            uId = i;
+        }
+        else
+        {
+            count++;
+        }
+        if (count > maxCount)
+        {
+            maxCount = count;
+            maxVoter = i;
+        }
+    }
+    return maxVoter;
+}
+
+int problem3()
+{
+    std::vector<int> testArr;
+    testArr.push_back(4);
+    testArr.push_back(4);
+    testArr.push_back(4);
+    testArr.push_back(4);
+    testArr.push_back(4);
+    testArr.push_back(4);
+    testArr.push_back(3);
+    testArr.push_back(3);
+    testArr.push_back(3);
+    testArr.push_back(3);
+    testArr.push_back(2);
+    testArr.push_back(1);
+    testArr.push_back(0);
+    testArr.push_back(0);
+    testArr.push_back(0);
+    testArr.push_back(1);
+    testArr.push_back(1);
+    testArr.push_back(1);
+    testArr.push_back(1);
+    QuickSort<int> q(testArr);
+    int ans = findWinner(q.returnSortedVector());
+    return ans;
+}
+
+int problem1()
 {
     std::vector<int> testArr;
     testArr.push_back(4);
@@ -37,14 +92,11 @@ void problem1()
     QuickSort<int> q(testArr);
     testArr = q.returnSortedVector();
     int uC = countUniqueElements(testArr);
-    if (uC != testArr.size())
-    {
-        std::cout << "Not unique" << std::endl;
-    }
+    return uC;
 }
 
-int main()
+TEST_CASE("Sorting Problems", "[sorting]")
 {
-    problem1();
-    return 0;
+    REQUIRE(problem1() == 5);
+    REQUIRE(problem3() == 4);
 }
